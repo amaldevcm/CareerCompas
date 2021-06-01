44b9ec28ec4153b85d11e246c58811b304412f7d
amaldevcm <amald1101@gmail.com>
2021-06-01 23:12:41 +0530

    Design update 2

--- app/src/main/java/com/example/careergudidenceapp/BookActivity.java
+++ app/src/main/java/com/example/careergudidenceapp/BookActivity.java
@@ -1,18 +1,18 @@
 package com.example.careergudidenceapp;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,6 +21,7 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class BookActivity extends AppCompatActivity {
+    CardView amazon_visit;
     TextView bname, bauth;
     ImageView bimg;
     Intent i;
@@ -40,10 +41,11 @@ public class BookActivity extends AppCompatActivity {
         bname= (TextView) findViewById(R.id.book_name);
         bauth = (TextView) findViewById(R.id.book_auth);
         bimg = (ImageView) findViewById(R.id.book_img);
+        amazon_visit = (CardView) findViewById(R.id.visit);
 
         String course = i.getStringExtra("Course");
         String book = i.getStringExtra("Book");
-//        Toast.makeText(this, book, Toast.LENGTH_SHORT).show();
+        final String[] url = new String[0];
 
         ref = FirebaseDatabase.getInstance().getReference("Stream").child("Engineering").child(course).child("Book").child(book);
         Query q = ref;
@@ -56,13 +58,20 @@ public class BookActivity extends AppCompatActivity {
                         .placeholder(R.mipmap.ic_launcher_round)
                         .error(R.mipmap.ic_launcher_round);
 
-
-
                 Glide.with(BookActivity.this).load(snapshot.child("Image").getValue().toString()).apply(options).into(bimg);
+                url[0] = snapshot.child("Link").getValue().toString();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) { }
+        });
+
+        amazon_visit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.intent.action.VIEW", Uri.parse(url[0]));
+                startActivity(intent);
+            }
         });
 
     }
